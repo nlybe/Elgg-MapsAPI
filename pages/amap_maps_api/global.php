0e28e3e2b32d2fcc32481cde536bd1c322e2eb17
Yaco <franco@reevo.org>
2016-12-16 15:39:11 -0300

    reimplemented the global map feature

--- pages/amap_maps_api/global.php
+++ pages/amap_maps_api/global.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Elgg AgoraMap Maps Api plugin
- * @package amap_maps_api 
+ * @package amap_maps_api
  */
 
-elgg_load_library('elgg:amap_maps_api');  
+elgg_load_library('elgg:amap_maps_api');
 // elgg_load_library('elgg:amap_maps_api_geocoder');  //OBS
 
 if (amap_ma_not_permit_public_access())	{
@@ -15,7 +15,7 @@ if (amap_ma_not_permit_public_access())	{
 $layers = amap_ma_get_map_layers();
 // Retrieve default layer
 $default_layer = amap_ma_get_map_default_layer();
-// Retrieve map width 
+// Retrieve map width
 $mapwidth = amap_ma_get_map_width();
 // Retrieve map height
 $mapheight = amap_ma_get_map_height();
@@ -25,6 +25,7 @@ $defaultlocation = amap_ma_get_map_default_location();
 $mapzoom = amap_ma_get_map_zoom();
 // Retrieve cluster feature
 $clustering = amap_ma_get_map_gm_clustering();
+
 $clustering_zoom = AMAP_MA_CUSTOM_CLUSTER_ZOOM;
 
 // get coords of default location
@@ -40,11 +41,11 @@ if (amap_ma_check_if_membersmap_gm_enabled()) {
 	$options1['metadata_name_value_pairs'] = array(array('name' => 'location', 'value' => '', 'operand' => '!='));
 	$users = elgg_get_entities_from_metadata($options1);
 	$indextable .= elgg_view('input/checkbox', array(
-		'name' => 'chbx_user', 
-		'id' => 'chbx_user', 
+		'name' => 'chbx_user',
+		'id' => 'chbx_user',
 		'checked' => true,
-	)).'<label for="chbx_user">'.elgg_echo('amap_maps_api:members').'</label>';	
-	
+	)).'<label for="chbx_user">'.elgg_echo('amap_maps_api:members').'</label>';
+
 	if ($users) {
 		foreach ($users as $entity) {
 			$entity = amap_ma_set_entity_additional_info($entity, 'name', 'description', $entity->location);
@@ -54,17 +55,17 @@ if (amap_ma_check_if_membersmap_gm_enabled()) {
 
 $groups = array();
 if (amap_ma_check_if_groupsmap_gm_enabled()) {
-	elgg_load_library('elgg:groupsmap');  
+	elgg_load_library('elgg:groupsmap');
 	$options2 = array('type' => 'group', 'full_view' => false);
 	$options2['limit'] = 0;
 	$options2['metadata_name_value_pairs'] = array(array('name' => 'location', 'value' => '', 'operand' => '!='));
 	$groups = elgg_get_entities_from_metadata($options2);
 	$indextable .= elgg_view('input/checkbox', array(
-		'name' => 'chbx_group', 
-		'id' => 'chbx_group', 
+		'name' => 'chbx_group',
+		'id' => 'chbx_group',
 		'checked' => true,
-	)).'<label for="chbx_group">'.elgg_echo('amap_maps_api:groups').'</label>';	
-	
+	)).'<label for="chbx_group">'.elgg_echo('amap_maps_api:groups').'</label>';
+
 	if ($groups) {
 		foreach ($groups as $entity) {
 			$entity = amap_ma_set_entity_additional_info($entity, 'name', 'briefdescription', groupsmap_get_group_location_str($entity));
@@ -79,16 +80,16 @@ if (amap_ma_check_if_agora_gm_enabled()) {
 		'subtype' => 'agora',
 		'limit' => 0,
 		'full_view' => false,
-		'view_toggle_type' => false 
+		'view_toggle_type' => false
 	);
 	$options3['metadata_name_value_pairs'] = array(array('name' => 'location', 'value' => '', 'operand' => '!='));
 	$ads = elgg_get_entities_from_metadata($options3);
 	$indextable .= elgg_view('input/checkbox', array(
-		'name' => 'chbx_agora', 
-		'id' => 'chbx_agora', 
+		'name' => 'chbx_agora',
+		'id' => 'chbx_agora',
 		'checked' => true,
-	)).'<label for="chbx_agora">'.elgg_echo('amap_maps_api:agora').'</label>';	
-	
+	)).'<label for="chbx_agora">'.elgg_echo('amap_maps_api:agora').'</label>';
+
 	if ($ads) {
 		foreach ($ads as $entity) {
 			$entity = amap_ma_set_entity_additional_info($entity, 'title', 'description');
@@ -102,66 +103,101 @@ if (amap_ma_check_if_pagesmap_gm_enabled()) {
 	$options4 = array(
 		'type' => 'object',
 		'subtype' => array('page','page_top'),
-		'full_view' => false,	
+		'full_view' => false,
 		'limit' => 0,
 	);
 	$pages = elgg_get_entities($options4);
-	
+
 	if ($pages) {
 		foreach ($pages as $entity) {
 			$entity = amap_ma_set_entity_additional_info($entity, 'title', 'description', $entity->location);
 		}
-	}	
-	
+	}
+
 	$indextable .= elgg_view('input/checkbox', array(
-		'name' => 'chbx_pages', 
-		'id' => 'chbx_pages', 
+		'name' => 'chbx_pages',
+		'id' => 'chbx_pages',
 		'checked' => true,
-	)).'<label for="chbx_pages">'.elgg_echo('amap_maps_api:pages').'</label>';	
+	)).'<label for="chbx_pages">'.elgg_echo('amap_maps_api:pages').'</label>';
 }
 
 if (!empty($indextable)) {
-	$indextable = '<div class="map_indextable">'.$indextable.'</div>';
+	$indextable = '<div id="map_indextable" class="map_indextable">'.$indextable.'</div>';
 }
- 
+
 $subtotal1 = array_merge($users, $groups);
 $subtotal2 = array_merge($subtotal1, $pages);
 $total = array_merge($subtotal2, $ads);
 
-$content = elgg_view('amap_maps_api/map_box', array(
-    'mapwidth'=>$mapwidth,
-    'mapheight'=>$mapheight,
-));
-$content .= elgg_view('amap_maps_api/map', array(
-	'entities'=>$total,
-	'defaultlocation'=>$defaultlocation,
-	'defaultzoom'=>$mapzoom,
-	'defaultcoords'=>$defaultcoords,
-	'clustering'=>$clustering,
-	'clustering_zoom'=>$clustering_zoom,
-	'layers'=>$layers,
-	'default_layer'=>$default_layer,
-	'osm_base_layer' => amap_ma_get_osm_base_layer(),
-));
+// print_r($total);
 
-// locad index table only if clustering is disable as it does'not work 
 if (!$clustering)    {
 	$content = $indextable.$content;
 }
-$side_vars = array();
+
+// get variables
+$s_location = $_GET["l"];
+$s_radius = (int) $_GET["r"];
+$s_keyword = $_GET["q"];
+$showradius = $_GET["sr"];
+// get initial load option from settings
+$initial_load = elgg_get_plugin_setting('initial_load', 'groupsmap');
+
+if ($s_location || $s_keyword) {
+    $search_radius_txt = '';
+    $s_radius = ($s_radius?$s_radius:AMAP_MA_DEFAULT_RADIUS);
+	$search_radius_txt = $s_radius;
+
+	// retrieve coords of location asked, if any
+	$coords = amap_ma_geocode_location($s_location);
+
+    if ($coords) {
+		$s_radius = amap_ma_get_default_radius_search($s_radius);
+		$search_location_txt = $s_location;
+        $s_lat = $coords['lat'];
+        $s_long = $coords['long'];
+
+		$title = elgg_echo('groupsmap:groups:nearby:search', array($search_location_txt));
+    }
+
+    // if special params asked, then forget the initial load option from settings
+    $initial_load = '';
+}
+
+// load the search form
+$body_vars = array();
+$body_vars['s_action'] = 'amap_maps_api/nearby_search';
+$body_vars['initial_location'] = $search_location_txt;
+$body_vars['initial_radius'] = $search_radius_txt;
+$body_vars['initial_keyword'] = $s_keyword;
+$body_vars['initial_load'] =  $initial_load;
+if ($user->location) {
+	$body_vars['my_location'] = $user->location;
+	if (isset($initial_load) && $initial_load == 'location') {
+		$body_vars['initial_location'] = $user->location;
+	}
+}
+$form_vars = array('enctype' => 'multipart/form-data');
+
+$content .=  elgg_view_form('amap_maps_api/nearby', $form_vars, $body_vars);
+$content .= elgg_view('amap_maps_api/map_box', array(
+    'mapwidth' => $mapwidth,
+    'mapheight' => $mapheight,
+));
+
+$sidebar = '';
+$layout = 'one_column';
 
 $params = array(
 	'content' => $content,
-	'sidebar' => '',
+	'sidebar' => $sidebar,
 	'title' => $title,
 	'filter_override' => '',
 );
 
-$body = elgg_view_layout('one_column', $params);
-
+$body = elgg_view_layout($layout, $params);
 
 echo elgg_view_page($title, $body);
-
 // release variables
 unset($users);
 unset($groups);
@@ -170,6 +206,3 @@ unset($pages);
 unset($subtotal1);
 unset($subtotal2);
 unset($total);
-
-
-
--- views/default/amap_maps_api/amap_search.js
+++ views/default/amap_maps_api/amap_search.js
@@ -2,23 +2,25 @@ define(function (require) {
     var elgg = require('elgg');
     var $ = require('jquery');
     require("amap_ma_oms_js");
-    
+
+
     // Initialize map vars
     var map_settings = require("amap_maps_api/settings");
-    var gm = google.maps;  
+    var gm = google.maps;
     var map;
     var mapTypeIds = [];
-    var markers = []; 	
+    var markers = [];
     var markerBounds = new google.maps.LatLngBounds();
     var mc;
     var circle = new google.maps.Circle();
-    
+
     // retrieve available map layers
     var layer_x = map_settings['layers'];
     $.each($.parseJSON(layer_x), function (item, value) {
-        mapTypeIds.push(value);  
-    });    
-   
+        mapTypeIds.push(value);
+
+    });
+
     $('#my_location').click(function () {
         if ($('#my_location').is(':checked')) {
             $('#autocomplete').val($('#user_location').val());
@@ -29,6 +31,7 @@ define(function (require) {
 
     $(document).ready(function () {
         // Initialize map vars
+
         infowindow = new google.maps.InfoWindow();
         var myLatlng = new google.maps.LatLng(map_settings['d_location_lat'],map_settings['d_location_lon']);
         var mapOptions = {
@@ -50,13 +53,17 @@ define(function (require) {
             tileSize: new google.maps.Size(256, 256),
             name: "OpenStreetMap",
             maxZoom: 18
-        }));  
-        
+        }));
+
         // trigger the search button for making the initial search
         setTimeout(function() {
             $("#nearby_btn").trigger('click');
+            console.log(markers);
+
         },10);
-        
+
+
+
         return false;
     });
 
@@ -68,22 +75,22 @@ define(function (require) {
             }
             markers = [];
             markerBounds = new google.maps.LatLngBounds();
-            
+
             if (map_settings['cluster']) {
                 mc.clearMarkers();
-            }            
+            }
         }
-        
+
         // reset search area, in case it exists
         circle.setMap(null);
-        
+
         var btn_text = $(this).val();
         $(this).prop('value', 'Searching...');
         $(this).css("opacity", 0.7);
-        
+
         // Spiderfier feature
         var oms = new OverlappingMarkerSpiderfier(map,{markersWontMove: true, markersWontHide: true, keepSpiderfied: true});
-        
+
         var s_location = $('#autocomplete').val();
         var s_radius = $('#s_radius').val();
         var s_keyword = $('#s_keyword').val();
@@ -109,7 +116,7 @@ define(function (require) {
             initSearchBtn(btn_text);
             return false;
         } else {
-            
+
             elgg.action(s_action, {
                 data: {
                     s_location: s_location,
@@ -117,31 +124,27 @@ define(function (require) {
                     s_keyword: s_keyword,
                     showradius: showradius,
                     initial_load: initial_load,
-                    noofusers: noofusers, 
-                    s_change_title: s_change_title, 
+                    noofusers: noofusers,
+                    s_change_title: s_change_title,
                     group_guid: group_guid
                 },
                 success: function (result) {
                     if (result.error) {
                         elgg.register_error(result.msg);
                     } else {
-                        if (result.change_title) {
-                            $('.elgg-heading-main').html(result.title);
-                        }
+
                         $('#map_location').html(result.location);
                         $('#map_radius').html(result.radius);
-                        
+
                         if (s_location) {
                             $('#s_radius').val(result.s_radius);
                         }
-                        if (change_title != 0) {
-                            $('.elgg-heading-main').html(result.title);
-                        }
-                        
+
+
                         if (initial_load == 'location') {
                             $('#my_location').prop('checked', true);
                         }
- 
+
                         if (s_location && result.s_location_lat && result.s_location_lng) {
                             //console.log('aa'+s_location+' - '+result.s_location);
                             var myLatlng = new google.maps.LatLng(result.s_location_lat,result.s_location_lng);
@@ -149,17 +152,17 @@ define(function (require) {
                                 map: map,
                                 position: myLatlng,
                                 icon: elgg.normalize_url('/mod/amap_maps_api/graphics/flag.png')
-                            }); 
-                            map.setCenter(marker.getPosition());  
-                           
+                            });
+                            map.setCenter(marker.getPosition());
+
                             google.maps.event.addListener(marker, 'click', function() {
                                 infowindow.setContent('Search address: '+s_location+'<br />Search radius: '+result.s_radius+' '+map_settings['unitmeas']);
                                 infowindow.open(map, this);
-                            });  
+                            });
                             markerBounds.extend(myLatlng);
 
                             oms.addMarker(marker);  // Spiderfier feature
-                            markers.push(marker);     
+                            markers.push(marker);
                             //console.log('aa'+showradius+' - '+result.s_radius);
                             if (showradius && result.s_radius_no > 0) {
                                 circle = new google.maps.Circle({
@@ -170,38 +173,42 @@ define(function (require) {
                                 });
                                 // Bind circle and marker
                                 circle.bindTo('center', marker, 'position');
-                                map.fitBounds(circle.getBounds());                            
+                                map.fitBounds(circle.getBounds());
                             }
                         }
-                        
+
                         var result_x = result.map_objects;
                         $.each($.parseJSON(result_x), function (item, value) {
-                            
+
                             var myLatlng = new google.maps.LatLng(value.lat,value.lng);
                             var marker = new google.maps.Marker({
                                 map: map,
                                 position: myLatlng,
                                 title: value.title,
                                 icon: value.map_icon,
+                                type: value.type,
                                 id: 'marker_'+value.guid
-                            });   
+                            });
+                            // console.log(value);
 
                             google.maps.event.addListener(marker, 'click', function() {
                                 infowindow.setContent('<div class="infowindow">'+value.info_window+'</div>');
                                 infowindow.open(map, this);
-                            });  
-                            
+                            });
+
                             oms.addMarker(marker);  // Spiderfier feature
                             markers.push(marker);
+                            console.log(markers);
 
                             if (!showradius)    {
                                 markerBounds.extend(myLatlng);
-                                map.fitBounds(markerBounds);                    
-                            }   
-                            
-                        });  
-                        
-                        if (map_settings['cluster']) {
+                                map.fitBounds(markerBounds);
+                            }
+
+                        });
+                        // If in Global Map
+
+                        if ((document.getElementById("map_indextable") == null) && (map_settings['cluster'])) {
                             mcOptions = {
                                 styles: [
                                     {
@@ -231,14 +238,14 @@ define(function (require) {
                                     }
                                 ],
                                 maxZoom: map_settings['cluster_zoom']
-                            };                    
+                            };
                             //init clusterer with your options
                             mc = new MarkerClusterer(map, markers, mcOptions);
                         }
-                        
+
                         if (result.sidebar) {
                             $('#map_side_entities').html(result.sidebar);
-                            
+
                             $('.map_entity_block').click(function() {
                                 var tmp_attr = $(this).find('a.entity_m');
                                 var object_id = tmp_attr.attr('id');
@@ -248,9 +255,9 @@ define(function (require) {
                                         google.maps.event.trigger(markers[i], 'click');
                                         break;
                                     }
-                                }                                
+                                }
                             });
-                            
+
                             $('.map_entity_block a.entity_m').click(function() {
                                 var object_id = $(this).attr('id');
                                 for (var i = 0; i < markers.length; i++) {
@@ -259,25 +266,72 @@ define(function (require) {
                                         google.maps.event.trigger(markers[i], 'click');
                                         break;
                                     }
-                                }                                
-                            });                            
-                        }                 
+                                }
+                            });
+                        }
+
+                        // If in Global Map
+                        if(document.getElementById("map_indextable") !== null) {
+                          $('#chbx_user').click(function() {
+                            if ($(this).is(':checked')) {
+                              var state = true;
+                            } else {
+                              var state = false;
+                            }
+                            for (var i = 0; i < markers.length; i++) {
+                                if (markers[i].type === "user") {
+                                    markers[i].setVisible(state);
+                                    break;
+                                }
+                            }
+                          });
+
+                          $('#chbx_group').click(function() {
+                            if ($(this).is(':checked')) {
+                              var state = true;
+                            } else {
+                              var state = false;
+                            }
+                            for (var i = 0; i < markers.length; i++) {
+                                if (markers[i].type === "group") {
+                                    markers[i].setVisible(state);
+                                    break;
+                                }
+                            }
+                          });
+
+                          $('#chbx_pages').click(function() {
+                            if ($(this).is(':checked')) {
+                              var state = true;
+                            } else {
+                              var state = false;
+                            }
+                            for (var i = 0; i < markers.length; i++) {
+                                if (markers[i].type === "objectpage_top") {
+                                    markers[i].setVisible(state);
+                                    break;
+                                }
+                            }
+                          });
+                        }
+
+
                     }
                 },
                 complete: function () {
                     // bring search button to normal
                     initSearchBtn(btn_text);
-                     
+
                     // set empty the initial input so it will not be used by search form
                     $('#initial_load').val('');
                  }
 
-            });            
+            });
         }
 
         return false;
     });
-    
+
 });
 
 function initSearchBtn(btn_text) {
